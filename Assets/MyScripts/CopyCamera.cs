c92571bb2a3f595b16d2eb90e13e5effcfde413b
Andrea Loriedo [el16al] <el16al@leeds.ac.uk>
2019-11-27 20:24:28 +0000

    Progress in fixing orthographic movements

--- Assets/MyScripts/CopyCamera.cs
+++ Assets/MyScripts/CopyCamera.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class CopyCamera : MonoBehaviour {
 
+	public Vector3 forward, right;
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
 	}
 }
--- Assets/MyScripts/MarbleController.cs
+++ Assets/MyScripts/MarbleController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MarbleController : MonoBehaviour {
 
+	public Vector3 forward, right;
 	Rigidbody rb;
 	[SerializeField] float speed;
 	[SerializeField] VirtualJoystick cursor;
@@ -12,6 +13,10 @@ public class MarbleController : MonoBehaviour {
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		forward = Camera.main.transform.forward;
+		forward.y = 0;
+		forward = Vector3.Normalize(forward);
+		right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
  	}
 
  // Update is called once per frame
@@ -20,10 +25,17 @@ public class MarbleController : MonoBehaviour {
 		mousePosX = Input.GetAxis("Mouse X") + mousePosX;
 		mousePosY = Input.GetAxis("Mouse Y") + mousePosY;
 
-		Vector3 movement = new Vector3(cursor.Horizontal() * speed, 0f, cursor.Vertical() * speed);
+		// Vector3 direction = new Vector3(cursor.Horizontal() * speed, 0f, cursor.Vertical() * speed);
+
+		Vector3 rightMovement = right * cursor.Horizontal() * speed;
+		Vector3 upMovement = forward * speed * cursor.Vertical() * speed;
+
+		Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+		transform.forward = heading;
 
 		transform.Rotate(mousePosX, 0f, mousePosY);
 
-		rb.AddForce(movement);
+		rb.AddForce(heading);
 	}
 }
