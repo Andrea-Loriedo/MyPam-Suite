929725c78e51ef586c5983ab20c05421d2b85524
Andrea Loriedo [el16al] <el16al@leeds.ac.uk>
2019-11-27 17:36:23 +0000

    Implemented virtual joystick

---
+++ Assets/MyScripts/MarbleController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleController : MonoBehaviour {
+
+	Rigidbody rb;
+	[SerializeField] float speed;
+	float mousePosX = 0;
+	float mousePosY = 0;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+ 	}
+
+ // Update is called once per frame
+	void FixedUpdate () {
+
+		mousePosX = Input.GetAxis("Mouse X") + mousePosX;
+		mousePosY = Input.GetAxis("Mouse Y") + mousePosY;
+
+		var x = Input.GetAxis ("Mouse X") * speed;
+		var y = Input.GetAxis ("Mouse Y") * speed;
+
+		Vector3 movement = new Vector3(mousePosX, 0f, mousePosY);
+
+		transform.Rotate(mousePosX, 0f, mousePosY);
+
+		rb.AddForce(movement);
+	}
+}
---
+++ Assets/VirtualJoystick.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class VirtualJoystick : MonoBehaviour , IDragHandler, IPointerUpHandler, IPointerDownHandler{
+
+	private Image background;
+	private Image knob;
+	private Vector3 input;
+
+	private void Start()
+	{
+		background = GetComponent<Image>(); 
+		knob = transform.GetChild(0).GetComponent<Image>();
+	}
+
+	public virtual void OnPointerDown(PointerEventData ped)
+	{
+		OnDrag(ped);
+	}
+
+	public virtual void OnPointerUp(PointerEventData ped)
+	{
+		input = Vector2.zero;
+		knob.rectTransform.anchoredPosition = Vector2.zero;
+	}
+
+	public virtual void OnDrag(PointerEventData ped)
+	{
+		Vector2 position;
+
+		if (RectTransformUtility.ScreenPointToLocalPointInRectangle(background.rectTransform, // the RectTransform to find a point inside
+																				ped.position, // screen space position
+																				ped.pressEventCamera, // the camera associated with the screen space position.
+																				out position)) // point in local space of the rect transform
+		{
+			position.x = (position.x / background.rectTransform.sizeDelta.x);
+			position.y = (position.y / background.rectTransform.sizeDelta.x);
+
+			input = new Vector2(position.x*2 + 1, position.y*2 - 1); 
+
+			if(input.magnitude > 1.0f)
+			{
+				input = input.normalized;
+			}
+			else
+			{
+				input = input;
+			}
+
+			knob.rectTransform.anchoredPosition = new Vector3(input.x * (background.rectTransform.sizeDelta.x/2), input.y * (background.rectTransform.sizeDelta.x/2));
+		}
+	}
+}
