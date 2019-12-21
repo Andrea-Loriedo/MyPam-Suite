3a6df63ac90c1162415cf33be706e3a204f215fe
Andrea-Loriedo <33459286+Andrea-Loriedo@users.noreply.github.com>
2019-12-21 13:09:15 +0100

    Solved assembly definition issues

---
+++ Assets/Tests/MarbleTestSuite.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MarbleTestSuite
+{
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator PlayModeTestingWithEnumeratorPasses() {
+        SetupScene();
+        yield return new WaitForSeconds(1);
+    }
+
+    void SetupScene()
+    {
+        SceneManager.LoadScene("Marble");   
+    }
+}
